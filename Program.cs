76aac3b55635213afeb721505d9b7b76c41e0d72
MirheydarA <a.mirheyder004@gmail.com>
2023-01-16 15:28:45 +0400

    task4

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿int[] numbers = { 4, 2, 3, 1 };
+int k; 
+for (int i = 0; i < numbers.Length; i++)
+{
+	for (int j = i; j < numbers.Length; j++)
+	{
+		if (numbers[i] > numbers[j] )
+		{
+			k = numbers[i];
+			numbers[i] = numbers[j];
+			numbers[j] = k;
+		}
+	}
+}
+for (int i = 0; i < numbers.Length; i++)
+{
+Console.WriteLine(numbers[i]);
+
+}
+
+
+
+
+
+////task 2
+//int[] numbers = { 4, 2, 3, 1 };
+//int sum = 0;
+//for (int i = 0; i < numbers.Length; i++)
+//{
+//sum = sum + numbers[i];
+
+
+//}
+//sum = sum / numbers.Length;
+//Console.WriteLine(sum);
+
+//task 3
+//int[] numbers = { 4, 2, 3, 1 };
+//for (int i = 0; i < numbers.Length; i++)
+//{
+
+//	for (int k = numbers.Length - 1; k >= 0; k--)
+//	{
+//		Console.WriteLine(numbers[k]);
+//	}
+//}
